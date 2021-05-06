f5461ad05ccb41cc7c075f4eeaabe9340fb58fd3
Genry Leyva <gleyvaglez@gmail.com>
2021-05-06 17:49:48 -0300

    fix test name file

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo ID=$(id -u)
 
 if [ -z "$ANSIBLE_PLAYBOOK" ]
 then
-   ANSIBLE_PLAYBOOK = "main-ansible-test.yml"
+   ANSIBLE_PLAYBOOK = "main-ansible.yml"
 fi
 echo ANSIBLE_PLAYBOOK=${ANSIBLE_PLAYBOOK}
 
